f1ef965d262c6dd76d48003364a8b5516fe82974
MrLoh <flojoto@hotmail.com>
2021-07-28 22:04:07 -0500

    add missing polyfills for jest

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,5 @@ require('@testing-library/jest-native/extend-expect');
 global.self = global;
 global.window = {};
 global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+global.Promise = require('promise-polyfill');
+global.FormData = require('form-data');
